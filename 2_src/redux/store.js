6f1734b4bdd0651361a94a02770b43f05b9222e0
teisha hsu <teishahsu@omusic.com.tw>
2021-09-01 12:07:51 +0800

    init

--- 2_src/redux/store.js
+++
@@ -1,8 +0,0 @@
-// createStore 一個 redux 中的方法，用於創建 store
-import { createStore } from 'redux'
-
-// store 會使用reducer
-import countReducer from './count_reducer'
-
-// 輸出 store
-export default createStore(countReducer)
